11400fc160d3fb7bfcb08bc000e4176e275d320f
하현 <ndg5778@gmail.com>
2022-07-18 15:39:03 +0900

    index.html

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 @app.route('/hi')
-def hi():
-    return 'hi!'
+def hellohtml():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
